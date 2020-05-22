82eb08c691e0064964d1e296d1563653d453df36
Jan Cavel <caveljan@gmail.com>
2020-05-22 09:59:08 +0300

    denatural-interpreted > dev: Callable, Interpreter | function

--- packages/denatural-interpreted/source/objects/Callable/index.ts
+++ packages/denatural-interpreted/source/objects/Callable/index.ts
@@ -1,4 +1,6 @@
 import Interpreter from '../Interpreter';
+import Environment from '../Environment';
+import * as Statement from '../Statement';
 
 
 
@@ -8,4 +10,48 @@ export interface Callable {
         args: any[],
     ) => any;
     arity: () => number;
+    toString: () => string;
 }
+
+
+class DenaturalFunction implements Callable {
+    private declaration: Statement.FunctionStatement;
+
+    constructor(
+        declaration: Statement.FunctionStatement,
+    ) {
+        this.declaration = declaration;
+    }
+
+    public call(
+        interpreter: Interpreter,
+        args: any[],
+    ) {
+        const environment = new Environment(interpreter.globals);
+
+        for (const parameter of this.declaration.params) {
+            environment.define(
+                parameter.lexeme,
+                parameter,
+            );
+        }
+
+        interpreter.executeBlock(
+            this.declaration.body,
+            environment,
+        );
+
+        return null;
+    }
+
+    public arity() {
+        return this.declaration.params.length;
+    }
+
+    public toString() {
+        return `<fn ${this.declaration.name.lexeme} >`;
+    }
+}
+
+
+export default DenaturalFunction;
