d62e87898d015a722fcb16dc099f3d0d5bf43353
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-05-24 11:46:22 +0300

    denatural-compiled > setup: package

---
+++ packages/denatural-compiled/source/library/debug.rs
@@ -0,0 +1,15 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_parens)]
+#![allow(unused_macros)]
+
+
+use crate::chunk::Chunk;
+
+
+
+pub fn disassemble_chunk(
+    chunk: &mut Chunk,
+) {
+    println!("disassembleChunk");
+}
